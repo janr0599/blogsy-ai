6910b880ee20b5a40e34dd97263d00727be41caf
janr0599 <javiernr0599@gmail.com>
2025-03-30 12:06:39 -0400

    update: stripe payment basic setup and db connection

--- components/home/Pricing.tsx
+++ components/home/Pricing.tsx
@@ -11,6 +11,11 @@ export default function Pricing() {
             description: "Get started with Blogsy",
             price: "9.99",
             items: ["3 Blog Posts", "3 Transcriptions"],
+            paymentLink: "https://buy.stripe.com/test_9AQdUSe1GeyGbZu7ss",
+            priceId:
+                process.env.NODE_ENV === "development"
+                    ? "price_1R8M89BpXPPZ8q082M68MGrK"
+                    : "",
         },
         {
             id: "pro",
@@ -18,6 +23,11 @@ export default function Pricing() {
             description: "All blog posts let's go!",
             price: "19.99",
             items: ["Unlimited Blog Posts", "Unlimited Transcriptions"],
+            paymentLink: "https://buy.stripe.com/test_6oEaIGg9Ocqy5B63cd",
+            priceId:
+                process.env.NODE_ENV === "development"
+                    ? "price_1R8MBJBpXPPZ8q08O2CwC5vd"
+                    : "",
         },
     ];
     return (
@@ -32,95 +42,86 @@ export default function Pricing() {
                     Choose the plan that works for you
                 </h3>
                 <div className="relative flex justify-center flex-col lg:flex-row items-center lg:items-stretch gap-8">
-                    {plans.map(({ name, price, description, items, id }, i) => (
-                        <div className="relative w-full max-w-lg" key={i}>
-                            <div
-                                className={cn(
-                                    "relative flex flex-col h-full gap-4 lg:gap-8 z-10 p-8 rounded-box border-[1px] border-gray-500/20 rounded-2xl",
-                                    id === "pro" && "border-violet-500 border-2"
-                                )}
-                            >
-                                <div className="flex justify-between items-center gap-4">
-                                    <div>
-                                        <p className="text-lg lg:text-xl font-bold capitalize">
-                                            {name}
-                                        </p>
-                                        <p className="text-base-content/80 mt-2">
-                                            {description}
-                                        </p>
+                    {plans.map(
+                        (
+                            {
+                                name,
+                                price,
+                                description,
+                                items,
+                                id,
+                                paymentLink,
+                            },
+                            i
+                        ) => (
+                            <div className="relative w-full max-w-lg" key={i}>
+                                <div
+                                    className={cn(
+                                        "relative flex flex-col h-full gap-4 lg:gap-8 z-10 p-8 rounded-box border-[1px] border-gray-500/20 rounded-2xl",
+                                        id === "pro" &&
+                                            "border-violet-500 border-2"
+                                    )}
+                                >
+                                    <div className="flex justify-between items-center gap-4">
+                                        <div>
+                                            <p className="text-lg lg:text-xl font-bold capitalize">
+                                                {name}
+                                            </p>
+                                            <p className="text-base-content/80 mt-2">
+                                                {description}
+                                            </p>
+                                        </div>
                                     </div>
-                                </div>
-                                <div className="flex gap-2">
-                                    <p className="text-5xl tracking-tight font-extrabold">
-                                        {price}
-                                    </p>
-                                    <div className="flex flex-col justify-end mb-[4px]">
-                                        <p className="text-xs text-base-content/60 uppercase font-semibold">
-                                            USD
+                                    <div className="flex gap-2">
+                                        <p className="text-5xl tracking-tight font-extrabold">
+                                            {price}
                                         </p>
-                                        <p className="text-xs text-base-content/60">
-                                            /month
-                                        </p>
+                                        <div className="flex flex-col justify-end mb-[4px]">
+                                            <p className="text-xs text-base-content/60 uppercase font-semibold">
+                                                USD
+                                            </p>
+                                            <p className="text-xs text-base-content/60">
+                                                /month
+                                            </p>
+                                        </div>
                                     </div>
-                                </div>
-                                <ul className="space-y-2.5 leading-relaxed text-base flex-1">
-                                    {items.map((item, i) => (
-                                        <li
-                                            className="flex gap-1 items-center"
-                                            key={i}
+                                    <ul className="space-y-2.5 leading-relaxed text-base flex-1">
+                                        {items.map((item, i) => (
+                                            <li
+                                                className="flex gap-1 items-center"
+                                                key={i}
+                                            >
+                                                <CheckIcon
+                                                    size={18}
+                                                ></CheckIcon>
+                                                <span>{item}</span>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                    <div className="space-y-2">
+                                        <Button
+                                            variant={"link"}
+                                            className={cn(
+                                                "border-2 rounded-full flex gap-2 bg-black text-gray-100 hover:no-underline",
+                                                id === "pro" &&
+                                                    "border-amber-300 "
+                                            )}
                                         >
-                                            <CheckIcon size={18}></CheckIcon>
-                                            <span>{item}</span>
-                                        </li>
-                                    ))}
-                                </ul>
-                                <div className="space-y-2">
-                                    <Button
-                                        variant={"link"}
-                                        className={cn(
-                                            "border-2 rounded-full flex gap-2 bg-black text-gray-100 hover:no-underline",
-                                            id === "pro" && "border-amber-300 "
-                                        )}
-                                    >
-                                        <Link
-                                            href={"/"}
-                                            className="flex gap-1 items-center"
-                                        >
-                                            Get Blogsy AI{" "}
-                                            <ArrowRight size={18} />
-                                        </Link>
-                                    </Button>
+                                            <Link
+                                                href={paymentLink}
+                                                className="flex gap-1 items-center"
+                                            >
+                                                Get Blogsy AI{" "}
+                                                <ArrowRight size={18} />
+                                            </Link>
+                                        </Button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    ))}
+                        )
+                    )}
                 </div>
             </div>
         </section>
     );
-}
-
-{
-    /* <div className="flex items-center justify-center gap-4 lg:gap-24">
-                {plans.map((plan) => (
-                    <div
-                        key={plan.name}
-                        className="flex flex-col gap-4 border p-4 rounded-lg shadow-lg w-80"
-                    >
-                        <h4 className="text-2xl font-bold">{plan.name}</h4>
-                        <p className="text-lg font-medium">
-                            {plan.description}
-                        </p>
-                        <p className="text-3xl font-bold">${plan.price}</p>
-                        <ul className="list-disc list-inside">
-                            {plan.items.map((item) => (
-                                <li key={item}>{item}</li>
-                            ))}
-                        </ul>
-                        <button className="mt-4 bg-purple-600 text-white py-2 px-4 rounded-lg hover:bg-purple-700 transition-colors duration-300">
-                            Get Started
-                        </button>
-                    </div>
-                ))}
-            </div> */
 }
