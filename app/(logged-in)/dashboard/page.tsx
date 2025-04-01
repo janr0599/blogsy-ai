32eac978524284ad98582c7499cb927052af4c3d
janr0599 <javiernr0599@gmail.com>
2025-04-01 09:14:34 -0400

    fix dashboard verification

--- app/(logged-in)/dashboard/page.tsx
+++ app/(logged-in)/dashboard/page.tsx
@@ -1,32 +1,39 @@
-import UpgradeYourPlan from "@/components/upload/UpgradeYourPlan";
-import { Badge } from "@/components/ui/badge";
 import BgGradient from "@/components/ui/BgGradient";
+import { Badge } from "@/components/ui/badge";
+import UpgradeYourPlan from "@/components/upload/UpgradeYourPlan";
+import UploadForm from "@/components/upload/UploadForm";
 import getDbConnection from "@/lib/db";
 import {
+    userExists,
     getPlanType,
     hasCancelledPlan,
     updateUser,
-    userExists,
 } from "@/lib/user-helpers";
 import { currentUser } from "@clerk/nextjs/server";
-import UploadForm from "@/components/upload/UploadForm";
+import { redirect } from "next/navigation";
 
 export default async function Dashboard() {
     const clerkUser = await currentUser();
+
+    if (!clerkUser) {
+        return redirect("/sign-in");
+    }
+
     const email = clerkUser?.emailAddresses?.[0].emailAddress ?? "";
 
     const sql = await getDbConnection();
 
+    //updatethe user id
     let userId = null;
     let priceId = null;
 
     const hasUserCancelled = await hasCancelledPlan(sql, email);
-
     const user = await userExists(sql, email);
+
     if (user) {
         //update the user_id in users table
-        if (clerkUser?.id) {
-            userId = clerkUser.id;
+        userId = clerkUser?.id;
+        if (userId) {
             await updateUser(sql, userId, email);
         }
 
@@ -36,15 +43,19 @@ export default async function Dashboard() {
     const { id: planTypeId = "starter", name: planTypeName } =
         getPlanType(priceId);
 
-    // const isFreePlan = planType === "starter";
     const isBasicPlan = planTypeId === "basic";
-    // const isProPlan = planTypeId === "pro";
+    const isProPlan = planTypeId === "pro";
+
+    // check number of posts per plan
+    const posts = await sql`SELECT * FROM posts WHERE user_id = ${userId}`;
+
+    const isValidBasicPlan = isBasicPlan && posts.length < 3;
 
     return (
         <BgGradient>
-            <section className="mx-auto max-w-6xl px-6 py-24 sm:py-32 lg:px-8">
+            <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8">
                 <div className="flex flex-col items-center justify-center gap-6 text-center">
-                    <Badge className="bg-gradient-to-r from-purple-700 to-pink-800 text-white px-4 py-1 text-lg font-semibold capitalize rounded-full">
+                    <Badge className="bg-gradient-to-r from-purple-700 to-pink-800 text-white px-4 py-1 text-lg font-semibold capitalize">
                         {planTypeName} Plan
                     </Badge>
 
@@ -53,31 +64,33 @@ export default async function Dashboard() {
                     </h2>
 
                     <p className="mt-2 text-lg leading-8 text-gray-600 max-w-2xl text-center">
-                        Upload your video or audio file and let our AI do the
-                        magic
+                        Upload your audio or video file and let our AI do the
+                        magic!
                     </p>
 
-                    <p className="mt-2 text-lg text-gray-600 max-w-2xl text-center leading-8">
-                        You get{" "}
-                        <span className="font-bold text-amber-600 bg-amber-100 px-2 py-1 rounded">
-                            {isBasicPlan ? "3" : "Unlimited"} blog posts
-                        </span>{" "}
-                        as part of the{" "}
-                        <span className="font-bold capitalize">
-                            {planTypeName}
-                        </span>{" "}
-                        plan.
-                    </p>
+                    {(isBasicPlan || isProPlan) && (
+                        <p className="mt-2 text-lg leading-8 text-gray-600 max-w-2xl text-center">
+                            You get{" "}
+                            <span className="font-bold text-amber-600 bg-amber-100 px-2 py-1 rounded-md">
+                                {isBasicPlan ? "3" : "Unlimited"} blog posts
+                            </span>{" "}
+                            as part of the{" "}
+                            <span className="font-bold capitalize">
+                                {planTypeName}
+                            </span>{" "}
+                            Plan.
+                        </p>
+                    )}
 
-                    {hasUserCancelled ? (
-                        <UpgradeYourPlan />
-                    ) : (
+                    {isValidBasicPlan || isProPlan || !hasUserCancelled ? (
                         <BgGradient>
                             <UploadForm />
                         </BgGradient>
+                    ) : (
+                        <UpgradeYourPlan />
                     )}
                 </div>
-            </section>
+            </div>
         </BgGradient>
     );
 }
